115506d7e47621a2b9f9065b8c6fdc4098062a05
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-03-06 14:10:47 +0530

    update media

--- Js/script.js
+++ Js/script.js
@@ -7,10 +7,12 @@ menuBtn.addEventListener("click", () => {
   if (open == true) {
     menuBtn.classList.remove("show");
     navMenu.style.right = "-100%";
+    document.body.classList.remove("no-scroll");
     open = false;
   } else {
     menuBtn.classList.add("show");
     navMenu.style.right = "0";
+    document.body.classList.add("no-scroll");
     open = true;
   }
 });
